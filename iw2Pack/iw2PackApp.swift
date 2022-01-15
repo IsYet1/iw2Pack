cc94a5c0dbe7bd8683038c6b71aeed4d19ded0c6
Don <don@isyet.com>
2022-01-15 11:18:08 -0600

    Initial Commit

---
+++ iw2Pack/iw2PackApp.swift
@@ -0,0 +1,17 @@
+//
+//  iw2PackApp.swift
+//  iw2Pack
+//
+//  Created by Don McKenzie on 15-Jan-22.
+//
+
+import SwiftUI
+
+@main
+struct iw2PackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
