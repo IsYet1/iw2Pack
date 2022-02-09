06431672fc6314ebd70b2fc0d179ff47c3188170
DonMck <donmck@amazon.com>
2022-02-09 12:41:46 -0600

    Some cleanup Still working ok.

--- iw2Pack/ItemLists/ItemCell.swift
+++ iw2Pack/ItemLists/ItemCell.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 struct ItemCell: View {
     let item: Item
-    
+    @State private var isOn = true
     @EnvironmentObject var store: Store<AppState>
+    
     struct Props {
         let allItemsDict: [String: Item]
         let attemptLogin: (String, String) -> Void
     }
     private func map(state: PackState) -> Props {
-        return Props(allItemsDict: state.allItemsDict,
-             attemptLogin: { store.dispatch(action: PackAttemptLogin(email: $0, password: $1))}
+        return Props(
+            allItemsDict: state.allItemsDict,
+            attemptLogin: { store.dispatch(action: PackAttemptLogin(email: $0, password: $1))}
                      
         )
     }
  
-    @State private var isOn = true
-    
     private func getItemName(itemId: String, allItemsDict: [String: Item]) -> String {
         if let dictItem = allItemsDict[itemId] {
             return dictItem.name!
@@ -35,10 +35,8 @@ struct ItemCell: View {
     var body: some View {
         let props = map(state: store.state.packAuthState)
         HStack {
-//            Text(item.itemId)
             Toggle(
                 getItemName(itemId:item.itemId!, allItemsDict: props.allItemsDict),
-//                "Checkbox 1",
                 isOn: $isOn
             )
               .toggleStyle(CheckboxToggleStyle(style: .circle))
